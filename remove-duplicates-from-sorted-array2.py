7211d9a8d42bab048a4cab35beddb708e91e5c87
burak <alamin.root@gmail.com>
2025-02-17 03:44:57 +0600

    Beats 25%

--- remove-duplicates-from-sorted-array2.py
+++ remove-duplicates-from-sorted-array2.py
@@ -1,3 +1,6 @@
+# https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii
+
+
 class Solution:
     def removeDuplicates(self, nums):
 
@@ -6,26 +9,15 @@ class Solution:
             return 0
 
         slow = 1
-        count = 1
-        FIXED_DUPLICATES = 2
 
         for fast in range(1, len(nums)):
 
-            if nums[fast] == nums[slow - 1]:
-                if count < FIXED_DUPLICATES:
-                    nums[slow] = nums[fast]
-                    slow += 1
-                count += 1
-
-            elif nums[fast] != nums[slow - 1]:
+            if slow == 1 or nums[fast] != nums[slow - 2]:
                 nums[slow] = nums[fast]
                 slow += 1
-                count = 1
 
         return slow
 
 
 s = Solution()
 print(s.removeDuplicates([0, 0, 1, 1, 1, 1, 2, 3, 3]))  # 7
-
-# Submitted Successfully
